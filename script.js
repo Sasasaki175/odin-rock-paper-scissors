4d962187691e8a301cbaf4a5273b5586f735202b
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-05-22 16:09:34 +0900

    Add function to play a round of rock paper scissors Added a function that returns the result depending on human input and computer choice. Added console.log to show the result and what was chosen by the human and computer. Added humanScore and computerScore variables that increments by 1 based on who won.

--- script.js
+++ script.js
@@ -19,9 +19,59 @@ function getHumanChoice() {
   }
 }
 
+function playRound(getHumanChoice, computerChoice) {
+  switch(getHumanChoice) {
+    case "rock":
+      if(computerChoice === "scissors") {
+        return "You win! Rock beats Scissors";
+        humanScore++;
+      } else if(computerChoice === "paper") {
+        return "You lose! Paper beats Rock";
+        computerScore++;
+      } else {
+        return "It's a tie";
+      }
+      break;
+    
+    case "paper":
+      if(computerChoice === "rock") {
+        return "You win! Paper beats Rock";
+        humanScore++;
+      } else if(computerChoice === "scissors") {
+        return "You lose! Scissors beats Paper";
+        computerScore++;
+      } else {
+        return "It's a tie";
+      }
+      break;
+    
+    case "scissors":
+      if(computerChoice === "paper") {
+        return "You win! Scissors beats paper";
+        humanScore++;
+      } else if(computerChoice === "rock") {
+        return "You lose! Rock beats Scissors";
+        computerScore++;
+      } else {
+        return "It's a tie";
+      }
+      break;
+    
+    default:
+      break;
+  }
+}
+
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 console.log("Hello World");
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+console.log(playRound(humanSelection, computerSelection));
+console.log("Human score: " + humanScore);
+console.log("Computer score: " + computerScore);
+
+console.log("Human choice was: " + humanSelection);
+console.log("Computer choice was: " + computerSelection);
