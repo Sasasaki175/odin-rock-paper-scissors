50422ddae25863eb302329ce7b8a19700dddc2a1
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-05-22 10:53:42 +0900

    Add function that returns 'rock' 'paper' or 'scissors'

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const randomNumber = Math.random();
+
+  if (randomNumber * 3 <= 1){
+    return "rock";
+  } else if (randomNumber * 3 <= 2){
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
